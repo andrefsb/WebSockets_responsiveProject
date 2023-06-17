d7713d26e45a40dd4d425380bfe1470e9a43aa0e
André Sampaio <andresampaio@id.uff.br>
2023-06-17 13:08:29 -0300

    Reading events

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,8 @@ import io from "./servidor.js";
 
 io.on("connection", (socket) =>{
     console.log("A client just connected. ID:", socket.id)
+
+    socket.on("texto_editor", (texto)=> {
+        console.log(texto)
+    })
   } );
