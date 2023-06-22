71ae915388627c139e7903ac0f0e5d1679527d65
André Sampaio <andresampaio@id.uff.br>
2023-06-22 15:23:58 -0300

    Rooms on Socket.IO

--- public/documento.js
+++ public/documento.js
@@ -1,6 +1,13 @@
-import { emitirTextoEditor } from "./socket-front-documento.js";
+import { emitirTextoEditor, selecionarDocumento } from "./socket-front-documento.js";
+
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get("nome");
+const tituloDocumento = document.getElementById("titulo-documento");
 
 const textoEditor = document.getElementById("editor-texto");
+tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+
+selecionarDocumento(nomeDocumento);
 
 textoEditor.addEventListener("keyup", () => {
     emitirTextoEditor(textoEditor.value)
--- src/socket-back.js
+++ src/socket-back.js
@@ -3,7 +3,12 @@ import io from "./servidor.js";
 io.on("connection", (socket) =>{
     console.log("A client just connected. ID:", socket.id)
 
+    socket.on("selecionar_documento", (nomeDocumento) =>{
+        socket.join(nomeDocumento);
+    })
+
     socket.on("texto_editor", (texto)=> {
         socket.broadcast.emit("texto_editor_clientes", texto);
     })
-  } );
+
+  });
