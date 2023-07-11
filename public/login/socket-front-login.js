545a524afb01f3a669d3049ac3f9d500da8d7cbd
André Sampaio <andresampaio@id.uff.br>
2023-07-10 21:25:28 -0300

    Authentication for user on login

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -4,4 +4,11 @@ function emitirAutenticarUsuario(dados){
     socket.emit("autenticar_usuario", dados);
 }
 
+socket.on("autenticacao_sucesso", ()=>{
+    alert("User successfully authenticated.")
+    window.location.href="/";
+});
+socket.on("autenticacao_erro", ()=>alert("Error authenticating user."));
+socket.on("usuario_nao_encontrado", ()=>alert("User not found."));
+
 export { emitirAutenticarUsuario };
